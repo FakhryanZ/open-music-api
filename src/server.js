fea1c2e90aeeeb98212b65e579de8dee9d1d9b19
Muhammad Fakhryan <fakhryanmuhammad@gmail.com>
2022-06-14 20:51:20 +0700

    authentication implemented

--- src/server.js
+++ src/server.js
@@ -1,15 +1,30 @@
 const Hapi = require('@hapi/hapi')
+const Jwt = require('@hapi/jwt')
+
 const albums = require('./api/albums')
-const songs = require('./api/songs')
 const AlbumsService = require('./service/AlbumsService')
-const SongsService = require('./service/SongsService')
 const AlbumsValidator = require('./validator/albums')
+
+const songs = require('./api/songs')
+const SongsService = require('./service/SongsService')
 const SongsValidator = require('./validator/songs')
+
+const users = require('./api/users')
+const UsersService = require('./service/UsersService')
+const UsersValidator = require('./validator/users')
+
+const authentications = require('./api/authentications')
+const AuthenticationsService = require('./service/AuthenticationsService')
+const TokenManager = require('./tokenize/TokenManager')
+const AuthenticationsValidator = require('./validator/authentications')
+
 require('dotenv').config()
 
 const init = async () => {
   const albumsService = new AlbumsService()
   const songsService = new SongsService()
+  const usersService = new UsersService()
+  const authenticationsService = new AuthenticationsService()
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -19,6 +34,28 @@ const init = async () => {
         origin: ['*'],
       },
     },
+  })
+
+  await server.register([
+    {
+      plugin: Jwt,
+    },
+  ])
+
+  server.auth.strategy('openmusic_jwt', 'jwt', {
+    keys: process.env.ACCESS_TOKEN_KEY,
+    verify: {
+      aud: false,
+      iss: false,
+      sub: false,
+      maxAgeSec: process.env.ACCESS_TOKEN_AGE,
+    },
+    validate: (artifacts) => ({
+      isValid: true,
+      credentials: {
+        id: artifacts.decoded.payload.id,
+      },
+    }),
   })
 
   await server.register([
@@ -35,6 +72,22 @@ const init = async () => {
       options: {
         service: songsService,
         validator: SongsValidator,
+      },
+    },
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    },
+    {
+      plugin: authentications,
+      options: {
+        authenticationsService,
+        usersService,
+        tokenManager: TokenManager,
+        validator: AuthenticationsValidator,
       },
     },
   ])
---
+++ src/validator/authentications/index.js
@@ -0,0 +1,34 @@
+const InvariantError = require('../../exceptions/InvariantError')
+const {
+  PostAuthenticationSchema,
+  PutAuthenticationSchema,
+  DeleteAuthenticationSchema,
+} = require('./schema')
+
+const AuthenticationValidator = {
+  validatePostAuthenticationPayload: (payload) => {
+    const validationResult = PostAuthenticationSchema.validate(payload)
+
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message)
+    }
+  },
+
+  validatePutAuthenticationPayload: (payload) => {
+    const validationResult = PutAuthenticationSchema.validate(payload)
+
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message)
+    }
+  },
+
+  validateDeleteAuthenticationPayload: (payload) => {
+    const validationResult = DeleteAuthenticationSchema.validate(payload)
+
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message)
+    }
+  },
+}
+
+module.exports = AuthenticationValidator
